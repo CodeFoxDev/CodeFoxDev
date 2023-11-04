f01214edc1b23f058c2ad793f7453cb7bdf350d6
Codefoxdev <admin@codefoxdev.com>
2023-11-04 15:13:30 +0100

    Changed merge conflict

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./js/projects.js";
 import { on } from "./js/spa.js"; "./js/spa.js";
 
@@ -39,71 +38,3 @@ function parseCssVal(inp, toPX) {
 
 initListeners();
 on("load", e => initListeners());
-=======
-import config from "/src/site.config.js";
-
-const nav = document.querySelector(".nav-progress");
-const navTop = document.querySelector(".nav-top");
-const projects = document.querySelector("div.projects");
-
-document.addEventListener("scroll", updateProgress);
-
-function updateProgress() {
-  const full = document.body.getBoundingClientRect();
-  const progress = (document.documentElement.scrollTop || document.body.scrollTop) / (full.height - window.innerHeight) * 100;
-  nav.style.setProperty("--width", `${progress}vw`);
-
-  if ((document.documentElement.scrollTop || document.body.scrollTop) < 600) navTop.classList.add("hidden");
-  else navTop.classList.remove("hidden");
-}
-
-(function() {
-  config.projects.forEach(project => {
-    const ele = document.createElement("div");
-    const src = project.src ?? `/src/projects/shot-${project.name}.png`;
-
-    ele.className = "project";
-    ele.id = `project-${project.name}`;
-
-    ele.innerHTML = /*html*/`
-      <img src="${src}" draggable="false">
-      <div class="info">
-        <h1>${project.name}</h1>
-        <div class="made-with">
-          <p>Made with:</p>
-          ${getDevIcons(project)}
-        </div>
-        <p>${project.description}</p>
-        <div class="buttons">
-          ${getButtons(project)}
-        </div>
-      </div>
-    `;
-
-    projects.appendChild(ele);
-  });
-
-  updateProgress();
-})();
-
-function getDevIcons(project) {
-  let res = "";
-
-  project.madeWith.forEach(item => {
-    res += /*html*/`
-      <img src="/src/icons/${item}.svg" draggable="false">
-    `;
-  });
-
-  return res;
-}
-
-function getButtons(project) {
-  let res = "";
-
-  if(project.live) res += /*html*/`<a class="button fill" href=${project.live} target="_blank">View live</a>`;
-  if(project.github) res += /*html*/`<a class="button" href=${project.github} target="_blank">View code</a>`;
-
-  return res;
-}
->>>>>>> 002b13a3d43ce511e4703387f93a7713bc3a1689
